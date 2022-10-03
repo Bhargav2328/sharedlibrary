455956309f4c373bcb7bc3904e3e651b8a13557e
Bhargav2328 <108718764+Bhargav2328@users.noreply.github.com>
2022-10-03 15:06:40 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,18 @@
+def newGit(repo)
+{
+  git "$(repo)"
+}
+
+def newMaven()
+{
+  sh 'mvn package'
+}
+
+def newDeploy(jobname,ip,context)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapp/${context}.war"
+}
+def runSelenium(jobname)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
